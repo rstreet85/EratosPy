c42e8be8ffed73ff1d84c6f2e60100c22c901c15
Robert <rob.streetman.dev@gmail.com>
2017-09-29 21:52:46 -0600

    eratos now separate method; making more pthonic version...

--- eratos3.py
+++ eratos3.py
@@ -4,27 +4,34 @@
 #@desc   Sieve of Eratosthenes, Python3: Accepts integer n, finds all prime integers from 1 to n; prints largest.
 #
 
+def eratos_old(n):
+    """ The older less-pythonic way of doing it """
+    #TODO:Stop looking for primes halfway between 0-n
+    mid = n / 2
+    primes = [True] * n
+
+    #Starting at multiples of 2...
+    #range or enumerate() (range does what xrange used to)
+    for num, isPrime in enumerate(primes, start=2):
+        #Don't bother finding multiples of x if x isn't a prime
+        if isPrime:
+            k = 2
+	      #Keep from checking for primes above n
+            while k * num < n :
+                primes[k * num] = False
+                k += 1
+
+    #Find first number still prime below n + 1
+    index = n - 1
+    while index > 0 and primes[index] != True:
+        index -= 1
+    
+    print("The largest prime was: {:,}".format(index))
+    
+def eratos(n):
+    """ Pythonic sieve of Eratosthenes """
+
 print("This script finds the largest prime integer from 0 to n.")
 n = int(input("Choose limit to search for primes: ")) + 1
 
-#Stop looking for primes halfway between 0-n
-mid = n / 2
-primes = [True] * n
-
-#Starting at multiples of 2...
-#for i in xrange(2,mid) :
-for num, isPrime in enumerate(primes, start=2):
-    #Don't bother finding multiples of x if x isn't a prime
-    if isPrime:
-        k = 2
-	  #Keep from checking for primes above n
-        while k * num < n :
-            primes[k * num] = False
-            k += 1
-
-#Find first number still prime below n + 1
-index = n - 1
-while index > 0 and primes[index] != True:
-    index -= 1
-    
-print("The largest prime was: {:,}".format(index))
+eratos_old(n)
